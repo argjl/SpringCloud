fafd1dc176c2e1cc30c15957ea5597e46910bd4a
ganesh kumar <ganeshargjl@gmail.com>
2024-09-18 21:40:40 +0530

    Second Commit

--- currency-calculation-service/src/main/java/com/springcloud/calculation/CurrencyCalculationServiceApplication.java
+++ currency-calculation-service/src/main/java/com/springcloud/calculation/CurrencyCalculationServiceApplication.java
@@ -2,10 +2,12 @@ package com.springcloud.calculation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.springcloud.calculation")
+@EnableDiscoveryClient
 public class CurrencyCalculationServiceApplication {
 
 	public static void main(String[] args) {
--- currency-calculation-service/src/main/java/com/springcloud/calculation/controller/CalculationController.java
+++ currency-calculation-service/src/main/java/com/springcloud/calculation/controller/CalculationController.java
@@ -1,11 +1,10 @@
 package com.springcloud.calculation.controller;
 
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,7 @@ public class CalculationController {
 
 	@Autowired
 	private CurrencyExchangeProxy proxy;
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
 
@@ -37,8 +36,6 @@ public class CalculationController {
 		return new CalculatedAmount(calculatedAmount.getId(), calculatedAmount.getFrom(), calculatedAmount.getTo(),
 				calculatedAmount.getConversionMultiple(), quantity, quantity * calculatedAmount.getConversionMultiple(),
 				calculatedAmount.getPort());
-//		System.out.println(calculatedAmount);
-//		return calculatedAmount;
 	}
 
 	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
@@ -50,29 +47,23 @@ public class CalculationController {
 		return new CalculatedAmount(calculatedAmount.getId(), calculatedAmount.getFrom(), calculatedAmount.getTo(),
 				calculatedAmount.getConversionMultiple(), quantity, quantity * calculatedAmount.getConversionMultiple(),
 				calculatedAmount.getPort());
-//		System.out.println(calculatedAmount);
-//		return calculatedAmount;
 	}
-	
+
 	@GetMapping("/currency-converter-server/from/{from}/to/{to}/quantity/{quantity}")
-    public CalculatedAmount calculateAmountServer(@PathVariable String from, @PathVariable String to, @PathVariable Long quantity) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
+	public CalculatedAmount calculateAmountServer(@PathVariable String from, @PathVariable String to,
+			@PathVariable Long quantity) {
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
 
-        // Load balanced RestTemplate will resolve the service name to a list of servers and balance across them
-        String url = "http://currency-exchange-service/currency-exchange/from/{from}/to/{to}";
-        CalculatedAmount calculatedAmount = restTemplate.getForObject(url, CalculatedAmount.class, uriVariables);
+		// Load balanced RestTemplate will resolve the service name to a list of servers
+		// and balance across them
+		String url = "http://currency-exchange-service/currency-exchange/from/{from}/to/{to}";
+		CalculatedAmount calculatedAmount = restTemplate.getForObject(url, CalculatedAmount.class, uriVariables);
 
-        return new CalculatedAmount(
-                calculatedAmount.getId(),
-                calculatedAmount.getFrom(),
-                calculatedAmount.getTo(),
-                calculatedAmount.getConversionMultiple(),
-                quantity,
-                quantity * calculatedAmount.getConversionMultiple(),
-                calculatedAmount.getPort()
-        );
-    }
+		return new CalculatedAmount(calculatedAmount.getId(), calculatedAmount.getFrom(), calculatedAmount.getTo(),
+				calculatedAmount.getConversionMultiple(), quantity, quantity * calculatedAmount.getConversionMultiple(),
+				calculatedAmount.getPort());
+	}
 
 }
